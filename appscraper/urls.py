91b14815965bdb7bd2849fc7323dee3d4d0f8d11
loharvikas <loharvikas13@gmail.com>
2021-08-06 20:05:33 +0530

    New

--- appscraper/urls.py
+++ appscraper/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     path("api/", include('api.urls')),
     re_path('.*', TemplateView.as_view(template_name="index.html")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
