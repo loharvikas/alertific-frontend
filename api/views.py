3b23f268ae9e3eff40b5c2f4f0f7e5054cc0bec6
loharvikas <loharvikas13@gmail.com>
2021-08-06 20:25:29 +0530

    CONTENT_TYPE

--- api/views.py
+++ api/views.py
@@ -86,3 +86,13 @@ class AppStoreAppListView(View):
             data.append(detail)
         return JsonResponse(data, safe=False)
 
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
