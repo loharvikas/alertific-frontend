38af6f4cbf60003e9303a5a1cb5daa2054f15142
loharvikas <loharvikas13@gmail.com>
2021-08-06 23:45:53 +0530

    c

--- appscraper/settings.py
+++ appscraper/settings.py
@@ -168,7 +168,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
