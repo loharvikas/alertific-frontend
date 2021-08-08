f4f53b715016723d6ca6e33573bfb9107a50ae36
loharvikas <loharvikas13@gmail.com>
2021-08-08 12:18:41 +0530

    Proxy to backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'https://alertific-backend.herokuapp.com',
       changeOrigin: true,
     })
   );
