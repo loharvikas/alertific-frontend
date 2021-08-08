87d2fda486041cc9c5e55aff40b986c419dbaa4c
loharvikas <loharvikas13@gmail.com>
2021-08-08 11:31:46 +0530

    Backend-delete

--- subscribe/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Subscriber, GooglePlay, AppStore, Country
-# Register your models here.
-admin.site.register(Subscriber)
-admin.site.register(GooglePlay)
-admin.site.register(AppStore)
-admin.site.register(Country)
